8160769b90aa205e8a24ceea3ec52e83a6657024
Mark Mueller <markmueller86@gmail.com>
2013-10-13 19:56:39 +0200

    Changed debian package dependencies and deleted unnecessary files

--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(wago-popup-dict):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="wago-popup-dict" command="/usr/bin/wago-popup-dict"
--- debian/wago-popup-dict.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the wago-popup-dict package
-#
-0 4	* * *	root	[ -x /usr/bin/wago-popup-dict_maintenance ] && /usr/bin/wago-popup-dict_maintenance
